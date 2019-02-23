72ec71d9a48e2528219728bd88a0118a669528a0
wak2luang <45333585+wak2luang@users.noreply.github.com>
2019-02-23 20:09:14 +0700

    Add files via upload

---
+++ user/setting.php
@@ -0,0 +1,115 @@
+<?php
+/* setting bot */
+$baca_plus=$ijo."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$turkis █▀▄ █── █─█ ▄▀▀────$ijo
+──── █▀█ █▀█ █─ █▀█────$turkis █─█ █─▄ █─█ ─▀▄────$ijo
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$turkis █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+creator:$kuning Shadow Bot$putih || code invite:$kuning F1Y5t1lzGx".$t;
+
+$msg_baca=$ijo."[•] Subscribe my channel => SHADOW BOT CHANNEL".$t."[!] Note : ".$t."[•] Jika terjadi kesalahan".$t."[•] berarti os hp lu di bawah lolipop".$t."[•] jika mau work harus di atas lolipop".$t;
+$stat_baca=true;
+/*money cube*/
+$money_cube=$turkis."
+ __  __                           ____      _
+|  \/  | ___  _ __   ___ _   _   / ___|   _| |__   ___
+$kuning| |\/| |/ _ \| '_ \ / _ \ | | | | |  | | | | '_ \ / _ \
+| |  | | (_) | | | |  __/ |_| | | |__| |_| | |_) |  __/
+$ijo|_|  |_|\___/|_| |_|\___|\__, |  \____\__,_|____/ \___|
+                         |___/ 
+
+$kuning Bot Money Cube (Paypal cash & free gift cards)
+$putih ====================================================
+$kuning [$putih creator :$ijo Shadow Bot$kuning ]$putih ||$kuning [$putih code invite :$ijo 59BQT$kuning ]
+$putih ====================================================".$t.$t;
+
+$msg_money=$ijo."[".$putih."•".$ijo."] Santuy gan ...".$t;
+$msg_cube=$ijo."[".$putih."•".$ijo."] Ngopi dulu biar gak stres".$t.$t;
+
+/*coin club*/
+$ccu=$red."
+  ___     _         $kn ___ _       _
+$red / __|___(_)_ __   $kn / __| |_   _| |__
+$red| |  / _ \ |  _ \ $kn | |  | | | | |  _ \
+$red| |__||_)| | | | | $kn| |__| | |_| | |_) |
+$red \___\___/_|_| |_| $kn \___|_|\____|____/";
+
+$msg_ccu=$ijo."[•] Note : Gunakan dengan baik";
+$stat_ccu=true;
+
+/*cash wallet*/
+$cash_wallet=$turkis."
+
+  ___          _     __          __    _ _      _
+ / __|__ _ ___| |__  \ \        / /__ | | | ___| |_
+$kuning| |  / _' / __| '_ \  \ \  /\  / / _ '| | |/ _ \  _|
+| |_| (_| \__ \ | | |  \ \/  \/ / (_| | | |  __/ |__
+$ijo \___\__,_|___/_| |_|   \__/\__/ \__,_|_|_|\___|\___|".$t;
+
+$stat_cash=true;
+/*kota*/
+$kota=$red."
+ _  __     _            ____        _
+| |/ /___ | |_ __ _    | __ )  ___ | |_
+$kuning| ' // _ \|  _/ _' |   |  _ \ / _ \|  _|
+| . \ (_) | |_ (_| | _ | |_) | (_) | |_
+$ijo|_|\_\___/ \__\__,_|(_)|____/ \___/ \__|".$t;
+
+$stat_kota=true;
+
+$flash="$red
+ ____ _           _        ____
+|  __| | __ _ ___| |__    / ___| ___
+| |_ | |/ _' / __| '_ \  | |  _ / _ \
+$putih|  _|| | (_| \__ \ | | | | |_| | ( ) |
+|_|  |_|\__,_|___/_| |_|  \____|\___/".$t;
+$msg_flash=$ijo."official youtube shadow bot channel".$r;
+$stat_flash=true;
+
+$yogo=$red." _    _                  
+\ \  / /                  _      ___     _
+$kuning \ \/ /___   __ _  ___   \ \    / (_) _,| | ___  ___
+  \  /  _ \ / _' |/ _ \   \ \  / /| |/ _  |/ _ \/ _ \
+$ijo   | | (_) | (_) | (_) |   \ \/ / | | (_| |  __/ (_) |
+   |_|\___/ \__, |\___/     \__/  |_|\_,__|\___|\___/
+$turkis            |___/".$t;
+
+$msgyg=$ijo."[".$kuning.">".$ijo."] Official Yt SHADOW BOT CHANNEL";
+$msgads=$ijo."[".$kuning.">".$ijo."] Jangan Lupa Subscribe ";
+$msgadsy=$putih."[".$ijo."√".$putih."] Note : ".$t.$ijo."[IND] Gunakan botnya jangan berlebihan takutnya nanti kena banned atau nanti gak bisa wd".$t."[ENG] use bots don't overdo fear later you get banned or can't withdraw.$t;
+
+
+
+$surfee=$kuning."
+ ___              __
+/ __| _   _ _ __ / _| ___  ___
+$red\__ \| | | | ,__| |_ / _ \/ _ \
+ __) | |_| | |  |  _|  __/  __/
+$putih|___/ \__,_|_|  |_|  \___|____|".$tt;
+
+
+
+
+$stat_wot=true;
+$donasi=false;
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
